cbabbba3050cd52b2cd804f7f6f48104d75f41f4
Guy Lyons <guylyons2@gmail.com>
2017-05-10 21:39:31 -0400

    add React

--- app/index.js
+++ app/index.js
@@ -1,12 +1,7 @@
-import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function component () {
-  var element = document.createElement('div');
-
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
