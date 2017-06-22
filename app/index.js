219ccca046cbca9a795368a67fa92746b6cff403
Guy <that.guy@mailhero.io>
2017-06-22 09:38:31 -0400

    add router and basic routes

--- app/index.js
+++ app/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-const app = (
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+
+const App = (
   <main>
-    Hello World!
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        
+        
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/contact" component={Contact}></Route>
+      </div>
+    </Router>
   </main>
 );
 
 ReactDOM.render(
-  app,
+  App,
   document.getElementById("root")
 );
