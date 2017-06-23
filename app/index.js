47368f70f5f3d0641ca56b34a40f245ecf65492f
Guy <that.guy@mailhero.io>
2017-06-23 09:23:49 -0400

    remove bootstrap

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { HashRouter } from 'react-router-dom';
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.scss";
 
 ReactDOM.render(
